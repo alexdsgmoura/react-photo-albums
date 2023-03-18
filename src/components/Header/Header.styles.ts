4146d9eef868d0ef6b04068ae957fc912d623e68
alexdsgmoura <alexdsgmoura@recife.pe.senac.br>
2023-03-18 01:07:58 -0300

    inicializando o desenvolvimento do header

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+import styleScript from '../../assets/fonts/StyleScript-Regular.ttf'
+
+export const Header = styled.header`
+    width: 100%;
+    height: 70px;
+
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+`
+
+export const Container = styled.div`
+    width: 100%;
+    max-width: 1140px;
+    height: 100%;
+
+    box-sizing: border-box;
+
+    display: flex;
+    align-items: center;
+
+    margin: auto;
+    padding: 0px 10px;
+`
+
+export const title = styled.h1`
+    margin: 0;
+
+    font-size: 1rem;
+`
+
+export const span = styled.span`
+    font-family: 'Style Script';
+
+    @font-face {
+        font-family: 'Style Script';
+        src: url(${styleScript}) format(truetype);
+    }
+
+`
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import * as C  from "./Header.styles"
+
+export const Header = () => {
+    return (
+        <C.Header>
+            <C.Container>
+                <C.title>ASG <C.span>ALBUMS</C.span></C.title>
+            </C.Container>
+        </C.Header>
+    )
+}
